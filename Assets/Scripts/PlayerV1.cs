29f2331dbb20fc7ca3c11b3ef8ef81bc5f488bc0
marvin-git-repo <marvin-git-repo@gmail.com>
2025-07-25 17:47:57 +0800

    added move script and animation

--- Assets/Scripts/PlayerV1.cs
+++ Assets/Scripts/PlayerV1.cs
@@ -43,9 +43,40 @@ public class PlayerV1 : MonoBehaviour
         p_moveAmt = p_moveAction.ReadValue<Vector2>();
         p_lookAmt = p_lookAction.ReadValue<Vector2>();
 
-        if(p_jumpAction.IsPressed())
+        if(p_jumpAction.WasPressedThisFrame())
         {
-
+            Jump();
         }
     }
+
+    public void Jump()
+    {
+        p_rigidbody.AddForceAtPosition(new Vector3(0, .125f, 0), Vector3.up, ForceMode.Impulse);
+        p_animator.SetTrigger("Jump");
+    }
+
+    private void FixedUpdate()
+    {
+        Walking();
+        Rotating();
+    }
+
+    private void Walking()
+    {
+        p_animator.SetFloat("Speed", p_moveAmt.y);
+        p_rigidbody.MovePosition(p_rigidbody.position + transform.forward * p_moveAmt.y * WalkSpeed * Time.deltaTime);
+
+    }
+
+    private void Rotating()
+    {
+        if(p_moveAmt.y != 0)
+        {
+            float rotationAmount = p_moveAmt.x * RotateSpeed * Time.deltaTime;
+            Quaternion deltaRotation = Quaternion.Euler(0, rotationAmount, 0);
+            p_rigidbody.MoveRotation(p_rigidbody.rotation * deltaRotation);
+        }
+    }
+
+
 }
